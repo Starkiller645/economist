b2ddb0176c1ebf345b21c85c61593a82c0fd0ad4
Tallie Tye <tallie@tallie.dev>
2023-05-01 15:02:21 +0100

    Added an 'initiator' field to the transaction DB

---
+++ migrations/20230501135932_Add_'initiator'_field_for_tracking.up.sql
@@ -0,0 +1,24 @@
+-- Add up migration script here
+
+DROP TABLE IF EXISTS transactions;
+DROP TABLE IF EXISTS currencies;
+
+CREATE TABLE IF NOT EXISTS currencies(
+	currency_id BIGSERIAL NOT NULL,
+	currency_code TEXT NOT NULL UNIQUE,
+	currency_name TEXT NOT NULL,
+	state TEXT NOT NULL,
+	circulation BIGINT NOT NULL,
+	reserves BIGINT NOT NULL,
+	PRIMARY KEY (currency_id)
+);
+
+CREATE TABLE IF NOT EXISTS transactions(
+	transaction_id BIGSERIAL NOT NULL,
+	transaction_date DATE NOT NULL,
+	currency_id BIGINT NOT NULL,
+	delta_circulation BIGINT,
+	delta_reserves BIGINT,
+	PRIMARY KEY (transaction_id),
+	FOREIGN KEY (currency_id) REFERENCES currencies(currency_id) ON DELETE CASCADE
+);
